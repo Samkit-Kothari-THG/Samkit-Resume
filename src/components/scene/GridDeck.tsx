9007db8c4e7334c72fc75ed6dacd2f2451ed892c
Samkit-Kothari-thg <Samkit.Kothari71@thg.com>
2026-06-07 11:31:30 +0530

    Fix Vercel build grid typing

--- src/components/scene/GridDeck.tsx
+++ src/components/scene/GridDeck.tsx
@@ -1,40 +1,26 @@
 import { useMemo } from "react";
 import * as THREE from "three";
 
-type GridLine = [[number, number, number], [number, number, number]];
-
-function buildGridLines(): GridLine[] {
-  const result: GridLine[] = [];
+function buildGridGeometry() {
+  const vertices: number[] = [];
 
   for (let i = -6; i <= 6; i += 1) {
-    result.push([
-      [-6, -1.62, i],
-      [6, -1.62, i]
-    ]);
-    result.push([
-      [i, -1.62, -6],
-      [i, -1.62, 6]
-    ]);
+    vertices.push(-6, -1.62, i, 6, -1.62, i);
+    vertices.push(i, -1.62, -6, i, -1.62, 6);
   }
 
-  return result;
+  const geometry = new THREE.BufferGeometry();
+  geometry.setAttribute("position", new THREE.Float32BufferAttribute(vertices, 3));
+
+  return geometry;
 }
 
 export function GridDeck() {
-  const lines = useMemo(buildGridLines, []);
+  const geometry = useMemo(buildGridGeometry, []);
 
   return (
-    <group>
-      {lines.map(([start, end], index) => {
-        const points = [new THREE.Vector3(...start), new THREE.Vector3(...end)];
-        const geometry = new THREE.BufferGeometry().setFromPoints(points);
-
-        return (
-          <line key={`${start.join("-")}-${end.join("-")}-${index}`} geometry={geometry}>
-            <lineBasicMaterial color={index % 2 ? "#223820" : "#254341"} transparent opacity={0.42} />
-          </line>
-        );
-      })}
-    </group>
+    <lineSegments geometry={geometry}>
+      <lineBasicMaterial color="#254341" transparent opacity={0.42} />
+    </lineSegments>
   );
 }
